dd489fa52e241f92a9b40b840f808c562d5f1320
Ajaykumaryd <ajaykumaryd2001@gmail.com>
2023-08-05 02:34:47 +0530

    updated

--- src/First.java
+++ src/First.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class First {
-}
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class First {
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); // Make sure to use the correct driver class name
+
+            String url = "jdbc:mysql://localhost:3306/yt";
+            String username = "root";
+            String password = "Ajay@2001";
+
+            // Use the connection to execute SQL queries or perform database operations
+            Connection connection = DriverManager.getConnection(url, username, password);
+
+
+//            connection.close(); // Close the connection when done
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    }
+
